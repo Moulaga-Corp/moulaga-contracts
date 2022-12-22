3253dbd285f8bf5685f4975da76a68009bde3aa3
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-12-22 17:38:59 +0100

    updated deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const MoulagaProtocol = await ethers.getContractFactory("MoulagaProtocol");
+  const protocolContract = await MoulagaProtocol.deploy();
+  await protocolContract.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const MoulagaSBT = await ethers.getContractFactory("MoulagaSBT");
+  const sbtContract = await MoulagaSBT.deploy(protocolContract.address);
+  await sbtContract.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Protocol: ${protocolContract.address}\nSBT: ${sbtContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
