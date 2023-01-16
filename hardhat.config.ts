eee2a6ba6fa09337613581a2aa6f6c73cded9f92
Paolo Manaois <paolo.manaois@yahoo.fr>
2023-01-16 18:31:04 +0100

    fuji config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,6 @@
+import { config as envConfig } from "dotenv";
+envConfig();
+import { get } from "env-var";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
@@ -6,6 +9,12 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [get("FUJI_ACCOUNT").required().asString()]
     }
   }
 };
